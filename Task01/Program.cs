e8f7e2913cbb43a4a33a801d3213357cc1e27a0b
Игорь <mc_grove@bk.ru>
2022-11-15 13:53:14 -0500

    add task01-02

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿int MinusMidlle(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 100;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int number1 = MinusMidlle(number);
+
+Console.WriteLine(number1);
---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Multiplicity(int number1, int number2) {
+    return number1 % number2;
+}
+
+int res = Multiplicity(num1, num2);
+if(res == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно {res}");
