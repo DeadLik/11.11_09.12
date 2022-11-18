c27ffd1d8d03e6f15dd382c4a8ecb4ab7e2806c5
Игорь <mc_grove@bk.ru>
2022-11-18 14:14:34 -0500

    add task07

---
+++ Task07/Program.cs
@@ -0,0 +1,19 @@
+﻿// 22. Напишите программу, которая
+// 1. принимает на вход число (N) и выдаёт таблицу
+// 2. квадратов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int numb)
+{
+    int count = 1;
+    while (count <= numb)
+    {
+        Console.WriteLine($"{count}  {count*count}");
+        count++;
+    }
+}
+
+if(number > 0) Square(number);
+else Console.WriteLine("Число не является натуральным");
