3ee8ca55e6423b87386cac511706e05746c9e09e
Игорь <mc_grove@bk.ru>
2022-11-18 13:34:28 -0500

    update task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -4,24 +4,45 @@
 // 3. выдаёт номер четверти плоскости, в которой находится эта
 // точка.
 
-Console.WriteLine("Введите координаты точки");
-Console.Write("X: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y: ");
-int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки");
+// Console.Write("X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-int Quarter(int xc, int yc)
+// int Quarter(int xc, int yc)
+// {
+//     if(xc > 0 && yc > 0) return 1;
+//     if(xc < 0 && yc > 0) return 2;
+//     if(xc < 0 && yc < 0) return 3;
+//     if(xc > 0 && yc < 0) return 4;
+//     return 0;
+// }
+
+// int quarter = Quarter(x, y);
+// string result = quarter > 0 
+//         ? $"Указанные координаты соответствуют четверти -> {quarter}"
+//         : "Введены некорректные координаты";
+
+// Console.WriteLine(result);
+
+// ------------------------------------------------------------------------
+
+// 18. Напишите программу, 
+// 1. которая по заданному номеру четверти, 
+// 2. показывает диапазон возможных координат точек 
+// 3. в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти ");
+string number = Console.ReadLine();
+
+string Range(string num)
 {
-    if(xc > 0 && yc > 0) return 1;
-    if(xc < 0 && yc > 0) return 2;
-    if(xc < 0 && yc < 0) return 3;
-    if(xc > 0 && yc < 0) return 4;
-    return 0;
+    if (num == "1") return "X > 0 и Y > 0";
+    if (num == "2") return "X < 0 и Y > 0";
+    if (num == "3") return "X < 0 и Y < 0";
+    if (num == "4") return "X > 0 и Y < 0";
+    return "Введены некорректные данные";
 }
-
-int quarter = Quarter(x, y);
-string result = quarter > 0 
-        ? $"Указанные координаты соответствуют четверти -> {quarter}"
-        : "Введены некорректные координаты";
-        
+string result = Range(number);
 Console.WriteLine(result);
