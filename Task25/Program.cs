af62c12f79951d82e15d770ada16a1f65a86c855
Игорь <mc_grove@bk.ru>
2022-12-02 14:14:52 -0500

    add new tasks

---
+++ Task25/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //2
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1); // 2- 3
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SumFirstRow(int[,] matrix)
+{
+    int summa = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        summa += matrix[i, i];
+    }
+    return summa;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+Console.WriteLine(SumFirstRow(array2D));
+
+
