9b2bf53cad3e8aefb6234bcae71232c611134c86
Игорь <mc_grove@bk.ru>
2022-12-09 14:09:27 -0500

    new tasks

---
+++ Task32/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int n)
+{
+    if (n == 0) return 0;
+    else
+        return n % 10 + SumNumbers(n / 10);
+}
+
+int res = SumNumbers(Math.Abs(number));
+Console.WriteLine(res);
+
---
+++ Task33/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите два числа число:");
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int PowerNumber(int numM, int numN)
+{
+    if (numN == 0) return 1;
+    return numM * PowerNumber(numM, numN - 1);
+}
+
+int res = PowerNumber(m, n);
+Console.WriteLine(res);
